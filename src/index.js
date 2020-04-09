bd9f29088a9a254d63ea7dc0fd3a7b16d6d4cc4b
shen zhen <sz05001@ly.com>
2020-04-09 11:13:09 +0800

    d

--- src/index.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("./datatype");
-require("./enum");
-require("./interface");
-var hello = 'hello typeScript';
-document.querySelectorAll('.app')[0].innerHTML = hello;

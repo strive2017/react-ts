9a0ee90fa6b9d15d4096ad1598fd249d58331656
shenzhen <strive0212@gmail.com>
2020-04-19 18:43:19 +0800

    indexindex

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,13 @@
 import './datatype'
 import './enum'
 import './interface'
+import './function'
+import './class'
+import './class-interface'
+import './generics'
+import './advanced'
+import './combination'
+
 
 let hello : string = 'hello typeScript'
 
